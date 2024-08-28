76f243049b59bd340518a661ca3565a8f197b4eb
cynthia <nwekecynthia2015@gmail.com>
2024-08-27 18:30:48 -0700

    intial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -88,34 +88,15 @@ const Navbar = () => {
             <div className="lg:hidden flex items-center">
                 <button onClick={toggleMenu}>
                     {menuOpen ? (
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="w-6 h-6"
-                        >
-                            <line x1="18" y1="6" x2="6" y2="18" />
-                            <line x1="6" y1="6" x2="18" y2="18" />
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6.75781 17.243L12.0008 12L17.2438 17.243M17.2438 6.75696L11.9998 12L6.75781 6.75696" stroke="#131D0E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
-                        ) : (
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            className="w-6 h-6"
-                        >
-                            <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M4 6h16M4 12h16m-7 6h7"
-                            />
-                        </svg>
+                    ) : (
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                       <path d="M20.5 7.5H3.5M20.5 12H6.5M20.5 16.5H3.5" stroke="#131D0E" stroke-linecap="round"/>
+                       </svg>
+                        
+                            
                     )}
                 </button>
             </div>
