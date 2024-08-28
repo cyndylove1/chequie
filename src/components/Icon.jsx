481485dbb1867e21b159fbe40625edef5d993acb
cynthia <nwekecynthia2015@gmail.com>
2024-08-27 20:14:30 -0700

    intial commit

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -3,7 +3,7 @@ import { PiStarFourFill } from "react-icons/pi";
 const Icon = () =>{
     return(
         <li className="flex items-start gap-4 pb-3">
-            <span className="bg-[#CCE8DA] text-green-600 rounded-full p-2 ">
+            <span className="bg-[#CCE8DA] text-[##008A48] rounded-full p-2 ">
                 <PiStarFourFill />
             </span>
         </li>
