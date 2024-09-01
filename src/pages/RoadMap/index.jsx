2f4d03fa21f2fc760023c894bf803b46a2364a3d
cynthia <nwekecynthia2015@gmail.com>
2024-09-01 15:51:09 -0700

    intial commit

--- src/pages/RoadMap/index.jsx
+++ src/pages/RoadMap/index.jsx
@@ -5,7 +5,7 @@ const Roadmap = () => {
     <section className="py-16 lg:mt-[6rem] mt-[5rem] ">
       <div className="mb-16 text-center">
         <p className="text-[15px] text-gray-500 uppercase">Roadmap</p>
-        <h2 className="lg:text-5xl text-2xl fw-medium lg:w-[40%] w-full mx-auto">
+        <h2 className="lg:text-5xl text-xl fw-medium lg:w-[40%] w-full mx-auto">
           We’re on a goal to<span className="text-[#008A48] -leading-[35px]"> 1,000 </span>Mates
         </h2>
       </div>
